3b2d4148d9e337007f83aa6d0418968f41d83034
Ayoub Belahcen <abelahce@e1r4p7.1337.ma>
2023-02-06 11:27:05 +0100

    siuuuu

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelahce <abelahce@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 05:47:50 by abelahce          #+#    #+#             */
+/*   Updated: 2023/02/06 11:22:03 by abelahce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void)
+{
+    std::cout << " default constructor has ben called !" << std::endl;
+    this->name = "nameless";
+    this->Hit_Points = 10;
+    this->Energy_Points = 10;
+    this->Attack_Damage = 0;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+    std::cout << " default deconstructor has ben called !" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name)
+{
+    std::cout << " default constructor has ben called !" << std::endl;
+    this->name = name;
+    this->Hit_Points = 10;
+    this->Energy_Points = 10;
+    this->Attack_Damage = 0;
+}
+
+ClapTrap::ClapTrap(ClapTrap &new_objet)
+{
+    std::cout << " sssssdefault constructor has ben called !" << std::endl;
+    this->name = new_objet.getName();
+    this->Hit_Points = new_objet.getHP();
+    this->Energy_Points = new_objet.getEP();
+    this->Attack_Damage = new_objet.getAD();
+}
+
+
+
+void ClapTrap::attack(const std::string& target)
+{
+    if (this->getEP() != 0 && this->getHP() != 0 )
+    {
+        this->setEP(this->getEP() + 1);
+        std::cout << "ClapTrap " << this->getName() << " attacks " << target << ", causing " << this->getAD() << " points of damage!" << std::endl;
+    }
+    else
+        std::cout << "ClapTrp " << this->getName() << " can't attack cause of the lack of either the Energy or Hit Points" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    this->setHP(this->getHP() - amount);
+    std::cout << "ClapTrap " << this->getName() << " has taken " << amount << " damage ! corrent HP is : " << this->getHP() << std::endl; 
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (this->getEP() != 0 && this->getHP() != 0 )
+    {
+        this->setEP(this->getEP() + 1);
+        this->setHP(this->getAD() + amount);
+        std::cout << "ClapTrap " << this->getName() << " has repaired with the amount : " << amount << "corrent HP : " << this->getHP() << std::endl;
+    }
+    else
+        std::cout << "ClapTrp " << this->getName() << " can't repaire itself cause of the lack of either the Energy or Hit Points" << std::endl;
+}
+
+
+// the getters functions
+
+std::string ClapTrap::getName(void)
+{
+    return (this->name);
+}
+
+int ClapTrap::getAD(void)
+{
+    return (this->Attack_Damage);
+}
+
+int ClapTrap::getEP(void)
+{
+    return (this->Energy_Points);
+}
+
+int ClapTrap::getHP(void)
+{
+    return (this->Hit_Points);
+}
+
+// the setters functions
+
+void ClapTrap::setName(std::string name)
+{
+    this->name = name;
+}
+
+void ClapTrap::setAD(int number)
+{
+    this->Attack_Damage = number;
+}
+
+void ClapTrap::setEP(int number)
+{
+    this->Energy_Points = number;
+}
+
+void ClapTrap::setHP(int number)
+{
+    this->Hit_Points = number;
+}
+
+// operators overloading 
+
+ClapTrap &ClapTrap::operator = (ClapTrap & s)
+{
+    this->name = s.getName();
+    this->Attack_Damage = s.getAD();
+    this->Energy_Points = s.getEP();
+    this->Hit_Points = s.getHP();
+    return (*this);
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelahce <abelahce@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 11:06:47 by abelahce          #+#    #+#             */
+/*   Updated: 2023/02/06 11:22:44 by abelahce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+    ClapTrap o;
+    std::cout << "----------------------\n";
+    ClapTrap siuu;
+    std::cout << "----------------------\n";
+    ClapTrap s("salam");
+    std::cout << "----------------------\n";
+    ClapTrap pp = s;
+    std::cout << "----------------------\n";
+
+    std::cout << o.getName() << std::endl;
+    std::cout << o.getHP() << std::endl;
+    std::cout << o.getAD() << std::endl;
+    std::cout << o.getEP() << std::endl;
+    o.setAD(20);
+    o.setName("jahad");
+    o.setHP(40);
+    o.setEP(200);
+    std::cout << o.getName() << std::endl;
+    std::cout << o.getHP() << std::endl;
+    std::cout << o.getAD() << std::endl;
+    std::cout << o.getEP() << std::endl;
+    std::cout << "----------------------\n";
+    std::cout << s.getName() << std::endl;
+    std::cout << s.getHP() << std::endl;
+    std::cout << s.getAD() << std::endl;
+    std::cout << s.getEP() << std::endl;
+    s.setAD(11);
+    s.setName("mazino");
+    s.setHP(111);
+    s.setEP(1111);
+    std::cout << s.getName() << std::endl;
+    std::cout << s.getHP() << std::endl;
+    std::cout << s.getAD() << std::endl;
+    std::cout << s.getEP() << std::endl;
+    std::cout << "----------------------\n";
+    std::cout << pp.getName() << std::endl;
+    std::cout << pp.getHP() << std::endl;
+    std::cout << pp.getAD() << std::endl;
+    std::cout << pp.getEP() << std::endl;
+    s.setAD(6);
+    s.setName("baam");
+    s.setHP(666);
+    s.setEP(6666);
+    std::cout << pp.getName() << std::endl;
+    std::cout << pp.getHP() << std::endl;
+    std::cout << pp.getAD() << std::endl;
+    std::cout << pp.getEP() << std::endl;
+    std::cout << "----------------------\n";
+    std::cout << siuu.getName() << std::endl;
+    std::cout << siuu.getHP() << std::endl;
+    std::cout << siuu.getAD() << std::endl;
+    std::cout << siuu.getEP() << std::endl;
+}
